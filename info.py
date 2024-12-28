7020fa8ccd3fe32653f65b74d2711cfd7a3daa52
Mohdali0786 <119667151+Mohdali0786@users.noreply.github.com>
2024-12-28 11:58:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ from Script import script
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION = environ.get('SESSION', 'Themoviesbot')
 API_ID = int(environ.get('API_ID', '6486910025'))
 API_HASH = environ.get('API_HASH', 'f4008abad73c887f6486910025cf137c')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
